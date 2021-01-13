cf2c00b867bc6db0d1fe650f4b2b92ae5c08c717
Sage Sypert <sage.sypert@gmail.com>
2021-01-12 20:05:36 -0800

    change background color to green, change background color of btn-basic to thistle, align text center

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-  $("ul").prepend("<li>Good Afternoon!</li>");
+  $("ul#user").prepend("<li>Good Afternoon!</li>");
+  $("ul#webpage").prepend("<li>Oh hi, I didn't see you there!</li>");
   });
 
   $("button#howareyou").click(function() {
-    $("ul").prepend("<li>How are you?</li>");
+    $("ul#user").prepend("<li>I just got here!</li>");
+    $("ul#webpage").prepend("<li>Well then, welcome!");
   });
 
   $("button#goodbye").click(function() {
-  $("ul").prepend("<li>See you later!</li>");
+  $("ul#user").prepend("<li>Already time to go, goodbye!</li>");
+  $("ul#webpage").prepend("<li>Ok then, see you later!</li>");
   });
 });
